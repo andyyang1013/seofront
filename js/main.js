a4a496fc16d90c97f09fc2bd1f4c2fa37b8a4a69
yz <yz@daicaihang.com>
2018-07-26 20:18:10 +0800

    commit code

--- js/main.js
+++ js/main.js
@@ -1,17 +1,86 @@
-$(window).scroll(function(){
-        var _top=$(window).scrollTop();
-        if(_top>180){
-            $(".menu").css({
-                'position':'fixed',
-                'top':'0'
-            });
-        }else{
-            $(".menu").css({
-                'position':'absolute',
-            });
+// 关遮罩
+$(".closeMask").click(function() {
+    $(".mask").hide();
+});
+// 返回
+$(".goBack").click(function() {
+    window.history.back();
+});
+var ajaxUrl = "http://10.10.67.5:9902/seoInformation"
+
+// 操作：新增-删除-上下架-修改-
+function ajaxBox(data, interface, backUrl) {
+    $.ajax({
+        type: 'POST',
+        dataType: "json",
+        url: ajaxUrl + interface,
+        data: data,
+        success: function(msg) {
+            if (msg.code == '0') {
+                layer.alert('操作成功', { icon: 6 }, function() {
+                    if (backUrl) {
+                        window.location.href = backUrl
+                    } else {
+                        layer.closeAll();
+                    }
+                });
+            } else {
+                layer.alert('操作失败，请重试', { icon: 5 });
+            }
         }
     });
-$(".tab").on("click","span",function(){
-    $(this).addClass("select").siblings("span").removeClass("select");
-    $(this).parents(".item").find(".item-content-list").eq($(this).index()).show().siblings("ul").hide();
-})
+}
+// 首页-频道
+function channel(){
+    $.ajax({
+        type: 'GET',
+        dataType: "json",
+        url: ajaxUrl + '/channel/listByPage',
+        success: function(msg) {
+            if (msg.code == '0') {
+               $(".content-channel").find('input[name$="title"]').val(msg.data[0].title)
+               $(".content-channel").find('input[name$="keyword"]').val(msg.data[0].keyword)
+               $(".content-channel").find('textarea[name$="desc"]').val(msg.data[0].desc)
+               // 修改
+               $(".mask-pd-edit").find('input[name$="title"]').val(msg.data[0].title)
+               $(".mask-pd-edit").find('input[name$="keyword"]').val(msg.data[0].keyword)
+               $(".mask-pd-edit").find('textarea[name$="desc"]').val(msg.data[0].desc)
+            } else {
+                layer.msg('数据异常，请重试', { icon: 5 });
+            }
+        }
+    });
+}
+// function ajaxYN(interface, backUrl, idNum) {
+//     layer.open({
+//         type: 1,
+//         area: ['250px', '150px'],
+//         shade: 0,
+//         content: '<p style="text-align: center;height:50px;line-height:50px;">确定进行此操作吗？</p>',
+//         btn: ['确定', '取消'],
+//         yes: function() {
+//             $.ajax({
+//                 url: ajaxUrl + interface,
+//                 type: "POST",
+//                 dataType: "json",
+//                 data: {
+//                     id: idNum
+//                 },
+//                 success: function() {
+//                     if (msg.code == '0') {
+//                         layer.alert('操作成功', { icon: 6 }, function() {
+//                             // window.location.href = backUrl
+//                         });
+//                     } else {
+//                         layer.alert('操作失败，请重试', { icon: 5 }, function() {
+//                             // layer.closeAll();
+//                         });
+//                     }
+//                 }
+//             })
+//         },
+//         btn2: function() {
+//             layer.closeAll();
+//         }
+//     })
+// }
