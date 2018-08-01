f8bf3fa7dea7e56932561a3e40f856d310e73779
lyx <1004392061@qq.com>
2018-08-01 14:21:54 +0800

    c

--- js/config_tpl.js
+++ js/config_tpl.js
@@ -1,1 +1,23 @@
-var ajaxUrl = "http://120.25.246.51:8003/news/api"
+var ajaxUrl = "http://120.25.246.51:8003/news/api";
+//设置cookie
+function setCookie(key,value,expires){
+    document.cookie=encodeURIComponent(key)+'='+encodeURIComponent(value)+';expires='+ddate(expires);
+  }
+    function ddate(expires){
+      var ddate=new Date()
+        ddate.setDate(ddate.getDate()+expires)
+        return ddate
+    }
+//读取cookie
+function getCookie(name){
+  var arrStr=document.cookie.split('; ');
+  //alert(arrStr)
+  for(var i=0;i<arrStr.length;i++){
+    var arr=arrStr[i].split('=')
+    //alert(arr[0]+'\n'+arr[1])
+    if(arr[0]==name){return decodeURIComponent(arr[1]) }
+  }
+ return ''
+}
+var tokenNum = getCookie('token');
+var ajaxHead = {"Token":tokenNum};
--- js/main.js
+++ js/main.js
@@ -1,27 +1,3 @@
-//设置cookie
-function setCookie(key,value,expires){
-    document.cookie=encodeURIComponent(key)+'='+encodeURIComponent(value)+';expires='+ddate(expires);
-  }
-    function ddate(expires){
-      var ddate=new Date()
-        ddate.setDate(ddate.getDate()+expires)
-        return ddate
-    }
-//读取cookie
-function getCookie(name){
-  var arrStr=document.cookie.split('; ');
-  //alert(arrStr)
-  for(var i=0;i<arrStr.length;i++){
-    var arr=arrStr[i].split('=')
-    //alert(arr[0]+'\n'+arr[1])
-    if(arr[0]==name){return decodeURIComponent(arr[1]) }
-  }
- return ''
-}
-
-var ajaxHead = {
-                "Token":tokenNum
-                }
 // 关遮罩
 $(".closeMask").click(function() {
     $(".mask").hide();
